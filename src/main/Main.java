24cac966531768a877db6fc41929f7602997aac0
Prathmesh Saini <sainiprathmesh@gmail.com>
2019-08-12 15:45:30 +0530

    Main.java: add statement for printing "Hello,World!"

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,10 +4,19 @@
  */
 package main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        for (int i = 0; i <= 10; i++) {
+            System.out.println(number + "X" + i + "=" + (number * i));
+
+        }
+
+    }
     }
 
 
-}
+
